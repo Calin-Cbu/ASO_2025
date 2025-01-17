240b196abfe6dd5b40138fa5f4b51c3fcbcd9e92
Calin-Cbu <calin.ciobanu.cbutech@gmail.com>
2025-01-17 09:43:10 +0200

    resolved ip bug

--- vlanScript.py
+++ vlanScript.py
@@ -6,19 +6,21 @@ def scan_vlan(ip_range):
     ether = Ether(dst="ff:ff:ff:ff:ff:ff")  # Adresă broadcast
     packet = ether / arp
 
+    print(f"Sending ARP requests to {ip_range}...")
     # Trimite pachetul și primește răspunsurile
-    result = srp(packet, timeout=2, verbose=0)[0]
+    result = srp(packet, timeout=5, verbose=1)[0]
 
     # Lista de dispozitive active
     devices = []
     for sent, received in result:
+        print(f"Received response from {received.psrc} with MAC {received.hwsrc}")
         devices.append({'ip': received.psrc, 'mac': received.hwsrc})
 
     return devices
 
 if __name__ == "__main__":
-    # Specifică range-ul de IP-uri al VLAN-ului (ex: 192.168.1.0/24)
-    ip_range = "10.8.1.0/24"
+    # Specific range-ul de IP-uri al VLAN-ului
+    ip_range = "10.8.11.8/24"
 
     print(f"Scanning VLAN in range {ip_range}...")
     active_devices = scan_vlan(ip_range)
